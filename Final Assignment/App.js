7114e84720d3ce725f1e5430099764a388f223b2
Arunsiva4 <116891956+Arunsiva4@users.noreply.github.com>
2023-07-30 02:09:22 +0530

    Add files via upload

---
+++ Final Assignment/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import data from './Component/App/data';
+import MobileCard from './Component/MobileCard/MobileCard';
+import './App.css';
+import Footer from "./Component/App/Footer";
+
+// eslint-disable-next-line
+import logo from './logo.svg';
+
+function showAdditional(additional) {
+  const greet="Greeting To React, ARUN'S REACT PROJECT ON MOBILES DETAILS, THIS IS displaying a greeting message "
+  alert(greet)
+
+  const alertInformation = Object.entries(additional)
+    .map(information => `${information[0]}: ${information[1]}`)
+    .join('\n');
+  alert(alertInformation)
+};
+function App() {
+  return (
+    <div className="wrapper">
+      <h1>Mobiles</h1>
+      {data.map(mob => (
+        <MobileCard 
+          key={mob.name}
+          name={mob.name}
+          Dimension={mob.Dimension}
+          Battery={mob.Battery}
+          price={mob.price}
+          additional={mob.additional}
+          showAdditional={showAdditional}
+        />
+      ))}
+      <Footer/>
+    </div>  
+      
+  )
+}
+
+export default App;
