ea548e1824a0ba92e084c14cff86b70ee8895f85
Arunsiva4 <116891956+Arunsiva4@users.noreply.github.com>
2023-08-01 19:56:30 +0530

    Add files via upload

---
+++ CPP/oddeven.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+int main(){
+	int a;
+	cout<<"Enter a Number:";
+	cin>>a;
+	if(a%2==0){
+		cout<<a<<" is an Even Number";
+	}
+	else{
+		cout<<a<<" is Odd Number";
+	}
+}
