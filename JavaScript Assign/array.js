b4310832e5c539b160e7edb3abdfe63e7c4ea684
Arunsiva4 <116891956+Arunsiva4@users.noreply.github.com>
2023-08-16 18:49:38 +0530

    Add files via upload

---
+++ JavaScript Assign/array.js
@@ -0,0 +1,10 @@
+const fruits=["apple", "banana", "orange", "grape", "mango"]
+const len_fruits= fruits.length;
+
+console.log(" Total number of fruits in the array:"+len_fruits)
+
+
+//Output
+
+//>node array
+// Total number of fruits in the array:5
