c615cb8741fbf2a90a041fe82501f93367f8587c
Arunsiva4 <116891956+Arunsiva4@users.noreply.github.com>
2024-02-17 19:45:16 +0530

    Create addsub.js

---
+++ AddSub/addsub.js
@@ -0,0 +1,9 @@
+const add = 10 + 2;
+console.log("The added value is " + add);
+const sub = 10 - 2;
+console.log("The subtracted value is " + sub);
+
+
+//output
+The added value is 12
+The subtracted value is 8
